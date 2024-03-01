408a2c14bed9652c6de47b3a9124fd9f16788a1b
Ayush Jaiswal <ayush45aj@gmail.com>
2024-03-01 22:30:37 +0530

    improvements

---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,37 @@
+import 'package:intern_tasks/models/food_item_model.dart';
+
+List<FoodItem> foodItems = [
+  FoodItem(
+      cost: 169,
+      name: "Farmhouse Pizza",
+      description: "Onion, Mushroom, Capsicum & Tomato",
+      imageUrl:
+      "https://lh3.googleusercontent.com/bbwf5LAF728glL7Bkeyx0S19pZaqvu_yC6AJS8MsFOK7lE_15TZbXYk7lyujI08jHNvE2ZFPk4tTsAqEWfpXCytHBSAVRHob-my1ng8=w512-rw",
+      isVeg: true,
+      isSpecial: true),
+  FoodItem(
+      cost: 180,
+      name: "Paneer Tikka Pizza",
+      description: "Savory fusion: Panner Tikka Pizza blends Indian flavors with cheesy delight",
+      imageUrl: "https://cdn.dotpe.in/longtail/store-items/8050593/mOpZvDFu.jpeg",
+      isVeg: true,
+      isSpecial: false),
+  FoodItem(
+      cost: 169,
+      name: "Tandoori Chicken BBQ Pizza",
+      description:
+      "A blend of smoky tandoori chicken and tangy BBQ sauce on a crispy pizza crust",
+      imageUrl:
+      "https://images.slurrp.com/prod/articles/ln83opwfdua.webp?impolicy=slurrp-20210601",
+      isVeg: false,
+      isSpecial: false),
+  FoodItem(
+      cost: 210,
+      name: "Tandoori Chicken BBQ Pizza",
+      description:
+      "A blend of smoky tandoori chicken and tangy BBQ sauce on a crispy pizza crust",
+      imageUrl:
+      "https://images.slurrp.com/prod/articles/ln83opwfdua.webp?impolicy=slurrp-20210601",
+      isVeg: false,
+      isSpecial: false),
+];
